8cd75932e2f2af2b3400641021b781267a81d83f
cah1983 <carolineho1983@gmail.com>
2021-08-16 14:17:20 +0100

    mapped some data to collector screen

---
+++ FrontEnd/src/DataFrontEnd/CollectorsData.js
@@ -0,0 +1,27 @@
+const collectorsData = [
+  {
+    id: 1,
+    category: 'Electronics',
+    name: 'floppy disk',
+    brand: 'IBM',
+    image: '../../Images/floppy_disk.jpg',
+    description: 'floppy disk 800KB capacity',
+    era: '1993',
+    condition: 'good',
+    price: '3'
+  },
+
+  {
+    id: 2,
+    category: 'Toys',
+    name: 'Ondamania',
+    brand: 'Toys R us',
+    image: '../../Images/floppy_disk.jpg',
+    description: 'favorite spring spiral toy from our childhood',
+    era: "90's",
+    condition: 'new',
+    price: '7'
+  }
+];
+
+module.exports = collectorsData;
--- SERVER/index.js
+++ SERVER/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const path = require('path');
-const collectors = require('./Data/Collectors')
+const collectors = require('../FrontEnd/src/DataFrontEnd/CollectorsData')
 
 
 // ROUTES
