d7ec9a7b5c5d705f2c43dd97c986b1c15cbe4096
cah1983 <carolineho1983@gmail.com>
2021-08-30 16:56:28 +0100

    changed logo added css stylesheet

--- FrontEnd/src/Components/Header.js
+++ FrontEnd/src/Components/Header.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../Styles/Header.css';
 
 function Header() {
   return (
-    <header className='header-wrapper'>
+    <header className='Header'>
       <div className='inline-flex container'>
         <div className='logo-wrapper'>
           <Link to='/'>
-            <img className='logo-header' src='Images/vintage-city-logo-black.svg' alt='VC logo' />
+            <img className='logo-header' src='Images/vintage-city-logo-white.svg' alt='VC logo' />
           </Link>
         </div>
 
-        <div className='inline-flex header-links-wrapper'>
+        <div className='inline-flex header-links'>
           <Link to='/Music'>Music</Link>
           <Link to='/TVshows'>TV Shows</Link>
           <Link to='/Collectors'>Collectors</Link>
